7db8d9b75cf770188cebf49ee304f5d02c6bf6f9
Manuel Bugallo <marvinody@gmail.com>
2019-10-20 02:30:40 -0400

    bugfix: fixes inaccurate filter

--- src/store/pics.js
+++ src/store/pics.js
@@ -22,7 +22,9 @@ export const getPics = () => async (dispatch, getState) => {
     type: LOAD_PICS,
     data: data
       .filter(pic => !pic.is_banned) // banned will never show up
-      .filter(pic => pic.file_url || pic.source), // and these let us display to user
+      .filter(pic => pic.file_url), // and this lets us display to user
+    // an empty file_url can be overcome later by checking source but not for right now
+    // would need a backend and I would like to keep this client only for the time being
   })
 }
 
