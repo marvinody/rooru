df591dba6943e406621fa91c0c77a276c98c0d5e
Manuel Bugallo <marvinody@gmail.com>
2019-10-24 23:49:23 -0400

    feat: adds image scaling css

--- src/Modal.js
+++ src/Modal.js
@@ -10,16 +10,21 @@ export function Modal(props) {
   if (!props.showModal) {
     return null
   }
-  // fk u then, I'll make a string that randomly generates so you never get a cache hit
 
-  const { file_url } = props.pic
+  const { file_url, image_height: height, image_width: width } = props.pic
   const proxied_url = file_url.replace('https://danbooru.donmai.us/data', 'http://booru-proxy.deploy.sadpanda.moe')
 
   return (
     <OutsideNotifier onOutsideClick={props.hideModal}>
       <div className='modal show-modal' onClick={props.hideModal}>
         <div className="modal-content">
-          <span className="close-button" onClick={props.hideModal}>&times;</span>
+
+          <div className='controls'>
+            <a className='' href='#'>
+              <span>Link</span>
+            </a>
+            <span className="close-button" onClick={props.hideModal}>&times;</span>
+          </div>
           <div className='img-resize'>
 
             <img src={proxied_url} onError={e => {
