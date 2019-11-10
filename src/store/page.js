332649b646ca1d3357c90198587e9cd5f553c4b3
Manuel Bugallo <marvinody@gmail.com>
2019-11-09 23:26:01 -0500

    feat: allows page to be set with query parameter

--- src/store/page.js
+++ src/store/page.js
@@ -1,8 +1,22 @@
+import history from '../history'
 const INCREMENT_PAGE = 'INCREMENT_PAGE'
 const DECREMENT_PAGE = 'DECREMENT_PAGE'
 export const RESET_PAGE = 'RESET_PAGE'
 
-const initialState = 0
+// take a look at the url and parse any existing page out for initial state
+const initialState = (function () {
+  if (!history.location.search) {
+    return 0
+  }
+  const qs = new URLSearchParams(history.location.search)
+  if (!qs.has('page')) {
+    return 0
+  }
+  const page = parseInt(qs.get('page'), 10)
+  // -1 because first page load increments before fetch
+  return isNaN(page) ? 0 : page - 1
+
+})()
 
 export const incPage = () => ({
   type: INCREMENT_PAGE,
